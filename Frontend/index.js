15fd2a7050dd3a505bd180bfbbafad83752ab73d
Khaled <74397286+khaled-0@users.noreply.github.com>
2022-01-26 15:14:02 +0000

    prettify frontend part 1

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,16 +1,88 @@
 const socket = io();
 
+const msgInput = document.getElementById('msgInput');
+const sendBtn = document.getElementById('sendBtn');
+const messagesHolder = document.querySelector(".messagesHolder")
+const messageList = document.getElementById("messageList")
+const scrollToBottomMessageBtn = document.getElementById("scrollToBottomMessageBtn")
+
+var doAutoScroll = true;
+
+sendBtn.innerHTML = getRandomEmoji()
+
+messagesHolder.onscroll = function() {
+  if (this.oldScroll > this.scrollTop) {
+    scrollToBottomMessageBtn.style.display = "block";
+    doAutoScroll = false;
+  } else if (this.scrollTop < this.oldScrollMax) {
+    scrollToBottomMessageBtn.style.display = "block";
+    doAutoScroll = false;
+  } else {
+    scrollToBottomMessageBtn.style.display = "none";
+    doAutoScroll = true;
+  }
+  this.oldScroll = this.scrollTop;
+  this.oldScrollMax = (this.scrollTop > (this.oldScrollMax == undefined ? 0 : this.oldScrollMax)) ? this.scrollTop : this.oldScrollMax
+}
+
 socket.on('message', text => {
 
-  const el = document.createElement('li');
-  el.innerHTML = text;
-  document.querySelector('ul').appendChild(el)
-
+  const msg = document.createElement('li');
+  msg.innerText = text;
+  messageList.appendChild(msg);
+  if (doAutoScroll) scrollToBottomMessage()
 });
 
-document.querySelector('button').onclick = () => {
+sendBtn.onclick = () => {
 
-  const text = document.querySelector('input').value;
+  if (sendBtn.innerText.length != 0) {
+    return sendMessage(sendBtn.innerText)
+  }
+  sendMessage();
+  msgInput.value = "";
+  document.querySelector('.sendBtn').className += " sendBtnEmoji";
+  sendBtn.innerText = getRandomEmoji()
+
+}
+document.querySelector('.msgInput').addEventListener('keypress', function(e) {
+  if (e.key === 'Enter') {
+    sendMessage();
+    msgInput.value = "";
+    document.querySelector('.sendBtn').className += " sendBtnEmoji";
+    sendBtn.innerText = getRandomEmoji()
+  }
+});
+
+msgInput.addEventListener("input", function(e) {
+  if (document.querySelector('.msgInput').value.length == 0) {
+    document.querySelector('.sendBtn').className += " sendBtnEmoji";
+    sendBtn.innerText = getRandomEmoji()
+  } else {
+    document.querySelector('.sendBtn').className = "sendBtn";
+    sendBtn.innerText = null;
+
+  }
+})
+
+function sendMessage(body) {
+  if (body != null) {
+    return socket.emit('message', body)
+  }
+  const text = msgInput.value;
+  if (text.length == 0) return;
   socket.emit('message', text)
+}
 
+function scrollToBottomMessage() {
+  messagesHolder.scroll({
+    top: messagesHolder.scrollHeight,
+    behavior: 'smooth'
+  });
+}
+
+function getRandomEmoji() {
+  var emojis = ["😀", "🤣", "😴", "😙"];
+  var random = Math.floor(Math.random() * emojis.length);
+  var emoji = emojis[random];
+  return emoji;
 }
